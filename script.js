c409217c2397bb35f71a86ae41a2111ef1a10300
Vladimir <nevajnooru@gmail.com>
2021-02-24 20:34:40 +0300

    first commiat all files

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// let answer = confirm("Тебе есть 18?");
+// console.log(answer);
+
+// let result = +prompt("Сколько тебе лет?", "");
+// console.log(result);
+
+const answers = [];
+
+answers[0] = prompt('Как тебя зовут?', '');
+answers[1] = prompt('Откуда ты?', '');
+answers[2] = prompt('Сколько тебе лет?', '');
+
+document.write(answers);
+console.log(answers);
+
+//
+// let cat = 'gucci';
+// console.log(`my cats name is ${cat}`);
